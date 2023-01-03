b84a7072a5946d7b85a317d359b71afbac27b588
fariassilva <alessandrofarias405@gmail.com>
2023-01-03 20:11:17 -0300

    Novas rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-const express = require('express')
+import * as express  from 'express'
+
 const app = express()
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World - 2 678888766')
+  res.status(201).send('GET /')
 })
+
+app.post('/', (req,res) => {
+  res.send('POST /')
+
+})
+
+app.put('/', (req,res) => {
+  res.send('PUT /')
+
+})
+
+app.patch('/', (req,res) => {
+  res.send('PATCH /')
+
+})
+
+app.post('/',(req,res) => {
+  res.send('POST /')
+
+})
+
+
+
+
+
+
+
 
 app.listen(3000, () => console.log('ONLINE http://localhost:3000'))
