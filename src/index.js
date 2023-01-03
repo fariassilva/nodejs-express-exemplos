a2b41c5d56d3010435372efd20d227721f8e7b59
fariassilva <alessandrofarias405@gmail.com>
2023-01-03 19:26:07 -0300

    Webpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.send('Hello World - 2 678888766')
 })
 
 app.listen(3000, () => console.log('ONLINE http://localhost:3000'))
