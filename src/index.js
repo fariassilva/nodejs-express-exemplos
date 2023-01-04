9cb495c8182d871d3271b8bd907e191c2e80ce83
fariassilva <alessandrofarias405@gmail.com>
2023-01-04 09:30:20 -0300

    Controller

--- src/index.js
+++ src/index.js
@@ -2,37 +2,12 @@ import * as express  from 'express'
 
 const app = express()
 
+import userController from './controller/userController'
+import postController from './controller/postController'
 
 
-app.get('/', (req, res) => {
-  res.status(201).send('GET /')
-})
-
-app.post('/', (req,res) => {
-  res.send('POST /')
-
-})
-
-app.put('/', (req,res) => {
-  res.send('PUT /')
-
-})
-
-app.patch('/', (req,res) => {
-  res.send('PATCH /')
-
-})
-
-app.post('/',(req,res) => {
-  res.send('POST /')
-
-})
-
-
-
-
-
-
+app.use('/user',userController)
+app.use('/post',postController)
 
 
 app.listen(3000, () => console.log('ONLINE http://localhost:3000'))
